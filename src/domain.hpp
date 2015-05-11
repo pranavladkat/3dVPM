d24e073186544a0f993ec7e9214d795724fe627a
Pranav Ladkat <pranavladkat@gmail.com>
2015-05-11 17:35:42 -0400

    documented domain.hpp briefly

--- src/domain.hpp
+++ src/domain.hpp
@@ -16,6 +16,7 @@ class Domain
 {
 private:
 
+    /** @brief maximum number of nodes in each direction  */
     int IMAX, JMAX, KMAX;
 
 public:
@@ -24,14 +25,19 @@ public:
 
     void set_domain_ranges(const int i,const int j,const int k);
 
+    /** @brief point data of the domain mesh file */
     std::vector<vector3d> nodes;
 
+    /** @brief returns maximum nodes in x direction */
     int get_IMAX() const ;
 
+    /** @brief returns maximum nodes in y direction */
     int get_JMAX() const ;
 
+    /** @brief returns maximum nodes in z direction */
     int get_KMAX() const ;
 
+    /** @brief returns total number of nodes */
     int n_nodes() const ;
 
 };
